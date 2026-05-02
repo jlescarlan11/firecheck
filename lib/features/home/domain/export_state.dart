92ae50d1c01180c2b5052dd6b383e5a88eb0ba23
John Lester Escarlan <jlescarlan11@gmail.com>
2026-05-02 13:34:51 +0800

    feat(export-validation): add ExportValidating and ExportValidationFailed states

--- lib/features/home/domain/export_state.dart
+++ lib/features/home/domain/export_state.dart
@@ -1,5 +1,6 @@
 // lib/features/home/domain/export_state.dart
 import 'package:firecheck/core/sync/shapefile/export/export_failure.dart';
+import 'package:firecheck/core/sync/shapefile/export/export_validation_result.dart';
 
 sealed class ExportState {
   const ExportState();
@@ -7,6 +8,15 @@ sealed class ExportState {
 
 class ExportIdle extends ExportState {
   const ExportIdle();
+}
+
+class ExportValidating extends ExportState {
+  const ExportValidating();
+}
+
+class ExportValidationFailed extends ExportState {
+  const ExportValidationFailed(this.errors);
+  final List<ExportLayerError> errors;
 }
 
 class ExportExporting extends ExportState {
